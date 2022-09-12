20ff08ae2b834d6173ea8bc4779597af31d45a43
dev-fa <franco.a.dev@proton.me>
2022-09-12 17:08:53 +0800

    Create functionality for number & operator bttns
    Clicking number buttons will now populate the display. Clicking an
    operator the first time will add to display. Clicking an operator
    after a second operand is inputted will calculate the operands
    and then populate the display with the answer along with the operator.

--- script.js
+++ script.js
@@ -20,23 +20,98 @@ function divide(num1, num2) {
 
 // operate = takes an operator and 2 numbers and then calls one of the above functions on the numbers
 function operate(operator, num1, num2) {
+    let answer;
     switch (operator) {
         case "+":
-            return add(num1, num2);
+            answer = add(num1, num2);
             break;
         case "-":
-            return subtract(num1, num2);
+            answer = subtract(num1, num2);
             break;
         case "*":
-            return multiply(num1, num2);
+            answer = multiply(num1, num2);
             break;
         case "/":
-            return divide(num1, num2);
+            answer = divide(num1, num2);
             break;
+    }
+    calculated.textContent = answer;
+    return parseInt(answer);
+}
+
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const plusBtn = document.getElementById("plus");
+const minusBtn = document.getElementById("minus");
+const multiplyBtn = document.getElementById("multiply");
+const divideBtn = document.getElementById("divide");
+const operateBtn = document.getElementById("operate");
+
+const calculations = document.getElementById("calculations");
+calculations.textContent = "0";
+const calculated = document.getElementById("calculated");
+
+
+// Display pressed number buttons on calculations display
+function displayCalculations(e) {
+    if (calculations.textContent === "0") calculations.textContent = "";
+    calculations.textContent += this.textContent;
+}
+
+const numberBtns = document.getElementsByClassName("number");
+
+for (i of numberBtns) {
+    i.addEventListener("click", displayCalculations);
+}
+
+
+// Store first number that is input when user presses an operator
+// Store second number if first number is already defined when user presses an operator or operate function
+// Set calculated answer as first number and remove second number
+function storeOperator(e) {
+    let solution = calculations.textContent;
+    let calculatedAnswer;
+    if (typeof firstOperand === "undefined") {
+        firstOperand = parseInt(calculations.textContent);
+        calculations.textContent += this.textContent;
+    } else if (typeof firstOperand != "undefined"){
+        calculationArray = calculations.textContent.split(/\+|-|\/|\*/g);
+        secondOperand = parseInt(calculationArray[1]);
+
+        if (solution.includes("+")) {
+            calculatedAnswer = operate("+", firstOperand, secondOperand);
+        } else if (solution.includes("-")) {
+            calculatedAnswer = operate("-", firstOperand, secondOperand);
+        } else if (solution.includes("*")) {
+            calculatedAnswer = operate("*", firstOperand, secondOperand);
+        } else if (solution.includes("/")) {
+            calculatedAnswer = operate("/", firstOperand, secondOperand);
+        }
+
+        calculations.textContent = calculatedAnswer + this.textContent;
+        firstOperand = calculatedAnswer;
+        secondOperand;
     }
 }
 
-console.log(operate("+", 9, 10)); // 19
-console.log(operate("-", 84, 14)); // 70
-console.log(operate("*", 6, 5)); // 30
-console.log(operate("/", 50, 5)); // 10
+let firstOperand;
+let secondOperand;
+let calculationArray = [];
+
+const operatorBtns = document.getElementsByClassName("operator");
+
+for (i of operatorBtns) {
+    i.addEventListener("click", storeOperator)
+}
+
+// Display calculated number from calculations to display
+
+
+// Save which operation has been chosen and then operate() on them when the user presses the “=” key
