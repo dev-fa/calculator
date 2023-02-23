34be874879e318bdef42ffb4c163b376ed6e6a55
dev-fa <franco.a.dev@proton.me>
2023-02-23 15:10:17 +0800

    Add enter key functionality

--- src/js/modules/ui.js
+++ src/js/modules/ui.js
@@ -36,6 +36,38 @@ export default class UI {
       UI.useDelete();
     } else if (key === 'reset') {
       UI.useReset();
+    } else if (key === 'enter') {
+      UI.useEnter();
+    }
+  }
+
+  static useNumbers(key) {
+    const numLastIndex = UI.screen.textContent.length - 1;
+    // If key is decimal
+    if (key === 'decimal') {
+      // Execute if decimal is not last index or already within string
+      if (
+        UI.screen.textContent.indexOf('.') !== numLastIndex &&
+        UI.screen.textContent.indexOf('.') === -1
+      ) {
+        UI.screen.textContent = `${calculator.getCurrentValue()}.`;
+      }
+      // If number has decimal before
+    } else if (UI.screen.textContent.substring(numLastIndex) === '.') {
+      calculator.currentValue = `${calculator.getCurrentValue()}.${
+        UI.numberKeys[key]
+      }`;
+      UI.screen.textContent = calculator.getCurrentValue();
+      // If current value is not zero
+    } else if (calculator.currentValue !== 0) {
+      calculator.currentValue = String(
+        calculator.getCurrentValue() + UI.numberKeys[key]
+      );
+      UI.screen.textContent = calculator.getCurrentValue();
+      // Replace current value if it is zero
+    } else {
+      calculator.currentValue = UI.numberKeys[key];
+      UI.screen.textContent = calculator.getCurrentValue();
     }
   }
 
@@ -68,33 +100,25 @@ export default class UI {
     UI.screen.textContent = calculator.getCurrentValue();
   }
 
-  static useNumbers(key) {
-    const numLastIndex = UI.screen.textContent.length - 1;
-    // If key is decimal
-    if (key === 'decimal') {
-      // Execute if decimal is not last index or already within string
-      if (
-        UI.screen.textContent.indexOf('.') !== numLastIndex
-        // && UI.screen.textContent.indexOf('.') === -1
-      ) {
-        UI.screen.textContent = `${calculator.getCurrentValue()}.`;
+  static useEnter() {
+    const functions = ['+', '-', '×', '/'];
+    for (let i = 0; i < functions.length; i += 1) {
+      if (UI.screen.textContent.indexOf(functions[i]) !== -1) {
+        const operands = calculator.currentValue.split(functions[i]);
+        let num;
+        [calculator.currentValue, num] = operands;
+        if (functions[i] === '+') {
+          calculator.add(num);
+        } else if (functions[i] === '-') {
+          calculator.subtract(num);
+        } else if (functions[i] === '×') {
+          calculator.multiply(num);
+        } else if (functions[i] === '/') {
+          calculator.divide(num);
+        }
+        UI.screen.textContent = `${calculator.getCurrentValue()}`;
+        return;
       }
-      // If number has decimal before
-    } else if (UI.screen.textContent.substring(numLastIndex) === '.') {
-      calculator.currentValue = `${calculator.getCurrentValue()}.${
-        UI.numberKeys[key]
-      }`;
-      UI.screen.textContent = calculator.getCurrentValue();
-      // If current value is not zero
-    } else if (calculator.currentValue !== 0) {
-      calculator.currentValue = String(
-        calculator.getCurrentValue() + UI.numberKeys[key]
-      );
-      UI.screen.textContent = calculator.getCurrentValue();
-      // Replace current value if it is zero
-    } else {
-      calculator.currentValue = UI.numberKeys[key];
-      UI.screen.textContent = calculator.getCurrentValue();
     }
   }
 
